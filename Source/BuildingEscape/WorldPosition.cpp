8a8710fc7c67b79d934094724794dc1c86b85b9e
Costigun <maximneveraa@gmail.com>
2020-12-31 19:44:38 +0600

    set my custom level as default, change project settings, add log messages

--- Source/BuildingEscape/WorldPosition.cpp
+++ Source/BuildingEscape/WorldPosition.cpp
@@ -19,8 +19,9 @@ void UWorldPosition::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	UE_LOG(LogTemp, Warning, TEXT("This is a Warning!"));
+	UE_LOG(LogTemp, Error, TEXT("ERROR!"));
+	UE_LOG(LogTemp, Display, TEXT("Just a Log!"));
 }
 
 
